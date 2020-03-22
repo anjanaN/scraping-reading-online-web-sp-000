6b3154cfc30ba55f04bbbfb1aa26c371e039b91b
Anjana Nair <anju740@gmail.com>
2020-03-22 20:17:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
