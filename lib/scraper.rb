ca129c036b061e988eb28d8be7c7e1bbd0783858
Anjana Nair <anju740@gmail.com>
2020-03-22 20:17:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,5 +5,5 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|
-  pts course.text.strip
+  puts course.text.strip
 end
