18ab8d8cd56ab5904289899aeaeb95ae95380c52
Anjana Nair <anju740@gmail.com>
2020-03-22 20:19:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,3 +7,5 @@ courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo00
 courses.each do |course|
   puts course.text.strip
 end
+
+p doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
